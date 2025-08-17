67843c543953493e453e78d9b254edc3ce6d436a
boj505 <b14004058@gmail.com>
2025-08-17 07:11:25 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        switzer: ['switzer', 'sans-serif'],
+        kola: ['kola', 'sans-serif'],
+        clash_display: ['Clash Display', 'sans-serif'],
+      },
+    },
+  },
+}
